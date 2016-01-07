4fd31f0d3fc62e3ed5c5afa718c5259092f31322
Cgl <cagilulusahin@gmail.com>
2016-01-07 13:28:05 +0200

    first commit

---
+++ src/main/java/nb/NbClassifier.java
@@ -0,0 +1,7 @@
+package nb;
+
+/**
+ * Created by cagil on 06/01/16.
+ */
+public class NbClassifier {
+}
---
+++ src/test/java/TestNaiveBayes.java
@@ -0,0 +1,5 @@
+/**
+ * Created by cagil on 07/01/16.
+ */
+public class TestNaiveBayes {
+}
